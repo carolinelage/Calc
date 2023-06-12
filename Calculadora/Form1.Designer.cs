3d3da6b7b2c1b850050394a739698b38d2cf335c
carolinelage <santos.caroline26@hotmail.com>
2023-06-12 16:58:26 -0300

    Base Calculadora

---
+++ Calculadora/Form1.Designer.cs
@@ -0,0 +1,361 @@
+﻿namespace Calculadora
+{
+  partial class Form1
+  {
+    /// <summary>
+    /// Required designer variable.
+    /// </summary>
+    private System.ComponentModel.IContainer components = null;
+
+    /// <summary>
+    /// Clean up any resources being used.
+    /// </summary>
+    /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+    protected override void Dispose(bool disposing)
+    {
+      if (disposing && (components != null))
+      {
+        components.Dispose();
+      }
+      base.Dispose(disposing);
+    }
+
+    #region Windows Form Designer generated code
+
+    /// <summary>
+    /// Required method for Designer support - do not modify
+    /// the contents of this method with the code editor.
+    /// </summary>
+    private void InitializeComponent()
+    {
+      System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+      this.tableLayoutPanel1 = new System.Windows.Forms.TableLayoutPanel();
+      this.btnCalcular = new System.Windows.Forms.Button();
+      this.button13 = new System.Windows.Forms.Button();
+      this.btn0 = new System.Windows.Forms.Button();
+      this.btn3 = new System.Windows.Forms.Button();
+      this.btn2 = new System.Windows.Forms.Button();
+      this.btn1 = new System.Windows.Forms.Button();
+      this.btn6 = new System.Windows.Forms.Button();
+      this.btn5 = new System.Windows.Forms.Button();
+      this.btn4 = new System.Windows.Forms.Button();
+      this.btn9 = new System.Windows.Forms.Button();
+      this.btn8 = new System.Windows.Forms.Button();
+      this.btn7 = new System.Windows.Forms.Button();
+      this.pnlDisplay = new System.Windows.Forms.Panel();
+      this.lblSinal = new System.Windows.Forms.Label();
+      this.lblFinal = new System.Windows.Forms.Label();
+      this.lblInicial = new System.Windows.Forms.Label();
+      this.lblResultado = new System.Windows.Forms.Label();
+      this.tblDisplayFunc = new System.Windows.Forms.TableLayoutPanel();
+      this.btnSoma = new System.Windows.Forms.Button();
+      this.btnC = new System.Windows.Forms.Button();
+      this.btnDivisao = new System.Windows.Forms.Button();
+      this.btnMulti = new System.Windows.Forms.Button();
+      this.btnSub = new System.Windows.Forms.Button();
+      this.pictureBox3 = new System.Windows.Forms.PictureBox();
+      this.pictureBox2 = new System.Windows.Forms.PictureBox();
+      this.pictureBox1 = new System.Windows.Forms.PictureBox();
+      this.tableLayoutPanel1.SuspendLayout();
+      this.pnlDisplay.SuspendLayout();
+      this.tblDisplayFunc.SuspendLayout();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+      this.SuspendLayout();
+      // 
+      // tableLayoutPanel1
+      // 
+      resources.ApplyResources(this.tableLayoutPanel1, "tableLayoutPanel1");
+      this.tableLayoutPanel1.Controls.Add(this.btnCalcular, 2, 3);
+      this.tableLayoutPanel1.Controls.Add(this.button13, 1, 3);
+      this.tableLayoutPanel1.Controls.Add(this.btn0, 0, 3);
+      this.tableLayoutPanel1.Controls.Add(this.btn3, 2, 2);
+      this.tableLayoutPanel1.Controls.Add(this.btn2, 1, 2);
+      this.tableLayoutPanel1.Controls.Add(this.btn1, 0, 2);
+      this.tableLayoutPanel1.Controls.Add(this.btn6, 2, 1);
+      this.tableLayoutPanel1.Controls.Add(this.btn5, 1, 1);
+      this.tableLayoutPanel1.Controls.Add(this.btn4, 0, 1);
+      this.tableLayoutPanel1.Controls.Add(this.btn9, 2, 0);
+      this.tableLayoutPanel1.Controls.Add(this.btn8, 1, 0);
+      this.tableLayoutPanel1.Controls.Add(this.btn7, 0, 0);
+      this.tableLayoutPanel1.Name = "tableLayoutPanel1";
+      // 
+      // btnCalcular
+      // 
+      resources.ApplyResources(this.btnCalcular, "btnCalcular");
+      this.btnCalcular.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btnCalcular.FlatAppearance.BorderSize = 0;
+      this.btnCalcular.Name = "btnCalcular";
+      this.btnCalcular.UseVisualStyleBackColor = true;
+      this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+      // 
+      // button13
+      // 
+      resources.ApplyResources(this.button13, "button13");
+      this.button13.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.button13.FlatAppearance.BorderSize = 0;
+      this.button13.Name = "button13";
+      this.button13.UseVisualStyleBackColor = true;
+      // 
+      // btn0
+      // 
+      resources.ApplyResources(this.btn0, "btn0");
+      this.btn0.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btn0.FlatAppearance.BorderSize = 0;
+      this.btn0.Name = "btn0";
+      this.btn0.UseVisualStyleBackColor = true;
+      this.btn0.Click += new System.EventHandler(this.btn0_Click);
+      // 
+      // btn3
+      // 
+      resources.ApplyResources(this.btn3, "btn3");
+      this.btn3.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btn3.FlatAppearance.BorderSize = 0;
+      this.btn3.Name = "btn3";
+      this.btn3.UseVisualStyleBackColor = true;
+      this.btn3.Click += new System.EventHandler(this.btn3_Click);
+      // 
+      // btn2
+      // 
+      resources.ApplyResources(this.btn2, "btn2");
+      this.btn2.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btn2.FlatAppearance.BorderSize = 0;
+      this.btn2.Name = "btn2";
+      this.btn2.UseVisualStyleBackColor = true;
+      this.btn2.Click += new System.EventHandler(this.btn2_Click);
+      // 
+      // btn1
+      // 
+      resources.ApplyResources(this.btn1, "btn1");
+      this.btn1.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btn1.FlatAppearance.BorderSize = 0;
+      this.btn1.Name = "btn1";
+      this.btn1.UseVisualStyleBackColor = true;
+      this.btn1.Click += new System.EventHandler(this.btn1_Click);
+      // 
+      // btn6
+      // 
+      resources.ApplyResources(this.btn6, "btn6");
+      this.btn6.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btn6.FlatAppearance.BorderSize = 0;
+      this.btn6.Name = "btn6";
+      this.btn6.UseVisualStyleBackColor = true;
+      this.btn6.Click += new System.EventHandler(this.btn6_Click);
+      // 
+      // btn5
+      // 
+      resources.ApplyResources(this.btn5, "btn5");
+      this.btn5.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btn5.FlatAppearance.BorderSize = 0;
+      this.btn5.Name = "btn5";
+      this.btn5.UseVisualStyleBackColor = true;
+      this.btn5.Click += new System.EventHandler(this.btn5_Click);
+      // 
+      // btn4
+      // 
+      resources.ApplyResources(this.btn4, "btn4");
+      this.btn4.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btn4.FlatAppearance.BorderSize = 0;
+      this.btn4.Name = "btn4";
+      this.btn4.UseVisualStyleBackColor = true;
+      this.btn4.Click += new System.EventHandler(this.btn4_Click);
+      // 
+      // btn9
+      // 
+      resources.ApplyResources(this.btn9, "btn9");
+      this.btn9.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btn9.FlatAppearance.BorderSize = 0;
+      this.btn9.Name = "btn9";
+      this.btn9.UseVisualStyleBackColor = true;
+      this.btn9.Click += new System.EventHandler(this.btn9_Click);
+      // 
+      // btn8
+      // 
+      resources.ApplyResources(this.btn8, "btn8");
+      this.btn8.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btn8.FlatAppearance.BorderSize = 0;
+      this.btn8.Name = "btn8";
+      this.btn8.UseVisualStyleBackColor = true;
+      this.btn8.Click += new System.EventHandler(this.btn8_Click);
+      // 
+      // btn7
+      // 
+      resources.ApplyResources(this.btn7, "btn7");
+      this.btn7.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btn7.FlatAppearance.BorderSize = 0;
+      this.btn7.Name = "btn7";
+      this.btn7.UseVisualStyleBackColor = true;
+      this.btn7.Click += new System.EventHandler(this.btn7_Click);
+      // 
+      // pnlDisplay
+      // 
+      resources.ApplyResources(this.pnlDisplay, "pnlDisplay");
+      this.pnlDisplay.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(176)))), ((int)(((byte)(188)))), ((int)(((byte)(152)))));
+      this.pnlDisplay.Controls.Add(this.lblSinal);
+      this.pnlDisplay.Controls.Add(this.lblFinal);
+      this.pnlDisplay.Controls.Add(this.lblInicial);
+      this.pnlDisplay.Controls.Add(this.lblResultado);
+      this.pnlDisplay.Name = "pnlDisplay";
+      // 
+      // lblSinal
+      // 
+      resources.ApplyResources(this.lblSinal, "lblSinal");
+      this.lblSinal.ForeColor = System.Drawing.SystemColors.ControlText;
+      this.lblSinal.Name = "lblSinal";
+      // 
+      // lblFinal
+      // 
+      resources.ApplyResources(this.lblFinal, "lblFinal");
+      this.lblFinal.ForeColor = System.Drawing.SystemColors.ControlText;
+      this.lblFinal.Name = "lblFinal";
+      // 
+      // lblInicial
+      // 
+      resources.ApplyResources(this.lblInicial, "lblInicial");
+      this.lblInicial.ForeColor = System.Drawing.SystemColors.ControlText;
+      this.lblInicial.Name = "lblInicial";
+      // 
+      // lblResultado
+      // 
+      resources.ApplyResources(this.lblResultado, "lblResultado");
+      this.lblResultado.ForeColor = System.Drawing.SystemColors.ControlText;
+      this.lblResultado.Name = "lblResultado";
+      // 
+      // tblDisplayFunc
+      // 
+      resources.ApplyResources(this.tblDisplayFunc, "tblDisplayFunc");
+      this.tblDisplayFunc.BackColor = System.Drawing.Color.Tan;
+      this.tblDisplayFunc.Controls.Add(this.btnSoma, 0, 4);
+      this.tblDisplayFunc.Controls.Add(this.btnC, 0, 0);
+      this.tblDisplayFunc.Controls.Add(this.btnDivisao, 0, 1);
+      this.tblDisplayFunc.Controls.Add(this.btnMulti, 0, 2);
+      this.tblDisplayFunc.Controls.Add(this.btnSub, 0, 3);
+      this.tblDisplayFunc.Name = "tblDisplayFunc";
+      // 
+      // btnSoma
+      // 
+      resources.ApplyResources(this.btnSoma, "btnSoma");
+      this.btnSoma.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btnSoma.FlatAppearance.BorderSize = 0;
+      this.btnSoma.Name = "btnSoma";
+      this.btnSoma.UseVisualStyleBackColor = true;
+      this.btnSoma.Click += new System.EventHandler(this.btnSoma_Click);
+      // 
+      // btnC
+      // 
+      resources.ApplyResources(this.btnC, "btnC");
+      this.btnC.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btnC.FlatAppearance.BorderSize = 0;
+      this.btnC.Name = "btnC";
+      this.btnC.UseVisualStyleBackColor = true;
+      this.btnC.Click += new System.EventHandler(this.btnC_Click);
+      // 
+      // btnDivisao
+      // 
+      resources.ApplyResources(this.btnDivisao, "btnDivisao");
+      this.btnDivisao.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btnDivisao.FlatAppearance.BorderSize = 0;
+      this.btnDivisao.Name = "btnDivisao";
+      this.btnDivisao.UseVisualStyleBackColor = true;
+      this.btnDivisao.Click += new System.EventHandler(this.btnDivisao_Click);
+      // 
+      // btnMulti
+      // 
+      resources.ApplyResources(this.btnMulti, "btnMulti");
+      this.btnMulti.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btnMulti.FlatAppearance.BorderSize = 0;
+      this.btnMulti.Name = "btnMulti";
+      this.btnMulti.UseVisualStyleBackColor = true;
+      this.btnMulti.Click += new System.EventHandler(this.btnMulti_Click);
+      // 
+      // btnSub
+      // 
+      resources.ApplyResources(this.btnSub, "btnSub");
+      this.btnSub.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.btnSub.FlatAppearance.BorderSize = 0;
+      this.btnSub.Name = "btnSub";
+      this.btnSub.UseVisualStyleBackColor = true;
+      this.btnSub.Click += new System.EventHandler(this.btnSub_Click);
+      // 
+      // pictureBox3
+      // 
+      this.pictureBox3.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.pictureBox3.Image = global::Calculadora.Properties.Resources.new_moon__2_;
+      resources.ApplyResources(this.pictureBox3, "pictureBox3");
+      this.pictureBox3.Name = "pictureBox3";
+      this.pictureBox3.TabStop = false;
+      // 
+      // pictureBox2
+      // 
+      this.pictureBox2.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.pictureBox2.Image = global::Calculadora.Properties.Resources.new_moon__1_;
+      resources.ApplyResources(this.pictureBox2, "pictureBox2");
+      this.pictureBox2.Name = "pictureBox2";
+      this.pictureBox2.TabStop = false;
+      // 
+      // pictureBox1
+      // 
+      this.pictureBox1.Cursor = System.Windows.Forms.Cursors.Hand;
+      this.pictureBox1.Image = global::Calculadora.Properties.Resources.new_moon;
+      resources.ApplyResources(this.pictureBox1, "pictureBox1");
+      this.pictureBox1.Name = "pictureBox1";
+      this.pictureBox1.TabStop = false;
+      this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+      // 
+      // Form1
+      // 
+      resources.ApplyResources(this, "$this");
+      this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+      this.Controls.Add(this.pictureBox3);
+      this.Controls.Add(this.pictureBox2);
+      this.Controls.Add(this.pictureBox1);
+      this.Controls.Add(this.tblDisplayFunc);
+      this.Controls.Add(this.pnlDisplay);
+      this.Controls.Add(this.tableLayoutPanel1);
+      this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
+      this.Name = "Form1";
+      this.Load += new System.EventHandler(this.Form1_Load);
+      this.tableLayoutPanel1.ResumeLayout(false);
+      this.pnlDisplay.ResumeLayout(false);
+      this.pnlDisplay.PerformLayout();
+      this.tblDisplayFunc.ResumeLayout(false);
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+      ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+      this.ResumeLayout(false);
+
+    }
+
+    #endregion
+
+    private System.Windows.Forms.TableLayoutPanel tableLayoutPanel1;
+    private System.Windows.Forms.Button btn7;
+    private System.Windows.Forms.Button btnCalcular;
+    private System.Windows.Forms.Button button13;
+    private System.Windows.Forms.Button btn0;
+    private System.Windows.Forms.Button btn3;
+    private System.Windows.Forms.Button btn2;
+    private System.Windows.Forms.Button btn1;
+    private System.Windows.Forms.Button btn6;
+    private System.Windows.Forms.Button btn5;
+    private System.Windows.Forms.Button btn4;
+    private System.Windows.Forms.Button btn9;
+    private System.Windows.Forms.Button btn8;
+    private System.Windows.Forms.Panel pnlDisplay;
+    private System.Windows.Forms.Label lblResultado;
+    private System.Windows.Forms.TableLayoutPanel tblDisplayFunc;
+    private System.Windows.Forms.Label lblSinal;
+    private System.Windows.Forms.Label lblFinal;
+    private System.Windows.Forms.Label lblInicial;
+    private System.Windows.Forms.Button btnSoma;
+    private System.Windows.Forms.Button btnC;
+    private System.Windows.Forms.Button btnDivisao;
+    private System.Windows.Forms.Button btnMulti;
+    private System.Windows.Forms.Button btnSub;
+    private System.Windows.Forms.PictureBox pictureBox1;
+    private System.Windows.Forms.PictureBox pictureBox2;
+    private System.Windows.Forms.PictureBox pictureBox3;
+  }
+}
+
---
+++ Calculadora/Form1.cs
@@ -0,0 +1,192 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculadora
+{
+  public partial class Form1 : Form
+  {
+    int v1, v2;
+    int soma;
+    public Form1()
+    {
+      InitializeComponent();
+    }
+
+    private void btnC_Click(object sender, EventArgs e)
+    {
+      lblResultado.Text = "";
+      lblInicial.Visible = false;
+      lblFinal.Visible = false;
+      lblSinal.Visible = false;
+    }
+
+    private void btn1_Click(object sender, EventArgs e)
+    {
+
+      var res = lblResultado.Text;
+      lblResultado.Text = res + 1;
+    }
+
+    private void btn2_Click(object sender, EventArgs e)
+    {
+
+      var res = lblResultado.Text;
+      lblResultado.Text = res + 2;
+    }
+
+    private void btn3_Click(object sender, EventArgs e)
+    {
+
+      var res = lblResultado.Text;
+      lblResultado.Text = res + 3;
+    }
+
+    private void btn4_Click(object sender, EventArgs e)
+    {
+
+      var res = lblResultado.Text;
+      lblResultado.Text = res + 4;
+    }
+
+    private void btn5_Click(object sender, EventArgs e)
+    {
+
+      var res = lblResultado.Text;
+      lblResultado.Text = res + 5;
+    }
+
+    private void btn6_Click(object sender, EventArgs e)
+    {
+
+      var res = lblResultado.Text;
+      lblResultado.Text = res + 6;
+    }
+
+    private void btn7_Click(object sender, EventArgs e)
+    {
+
+      var res = lblResultado.Text;
+      lblResultado.Text = res + 7;
+    }
+
+    private void btn8_Click(object sender, EventArgs e)
+    {
+
+      var res = lblResultado.Text;
+      lblResultado.Text = res + 8;
+    }
+
+    private void btn9_Click(object sender, EventArgs e)
+    {
+
+      var res = lblResultado.Text;
+      lblResultado.Text = res + 9;
+    }
+
+    private void btn0_Click(object sender, EventArgs e)
+    {
+      var res = lblResultado.Text;
+      lblResultado.Text = res + 0;
+    }
+
+    
+    private void btnCalcular_Click(object sender, EventArgs e)
+    {
+      if(lblSinal.Text == "+")
+      {
+        v2 = Convert.ToInt32(lblResultado.Text);
+        lblFinal.Visible = true;
+        lblFinal.Text = v2.ToString();
+
+        soma = v1 + v2;
+        lblResultado.Text = soma.ToString();
+      }else if(lblSinal.Text == "-")
+      {
+        v2 = Convert.ToInt32(lblResultado.Text);
+        lblFinal.Visible = true;
+        lblFinal.Text = v2.ToString();
+
+        soma = v1 - v2;
+        lblResultado.Text = soma.ToString();
+      }
+      else if(lblFinal.Text == "*")
+      {
+        v2 = Convert.ToInt32(lblResultado.Text);
+        lblFinal.Visible = true;
+        lblFinal.Text = v2.ToString();
+
+        soma = v1 * v2;
+        lblResultado.Text = soma.ToString();
+      }
+      else if(lblFinal.Text == "/")
+      {
+        v2 = Convert.ToInt32(lblResultado.Text);
+        lblFinal.Visible = true;
+        lblFinal.Text = v2.ToString();
+
+        soma = v1 / v2;
+        lblResultado.Text = soma.ToString();
+      }
+    }
+    private void btnSoma_Click(object sender, EventArgs e)
+    {
+      v1 = Convert.ToInt32(lblResultado.Text);
+      lblResultado.Text = "";
+      lblSinal.Visible = true;
+      lblSinal.Text = "+";
+      lblInicial.Visible = true;
+      lblInicial.Text = v1.ToString();
+    }
+
+    private void btnSub_Click(object sender, EventArgs e)
+    {
+      v1 = Convert.ToInt32(lblResultado.Text);
+      lblResultado.Text = "";
+      lblSinal.Visible = true;
+      lblSinal.Text = "-";
+      lblInicial.Visible = true;
+      lblInicial.Text = v1.ToString();
+    }
+
+    private void btnMulti_Click(object sender, EventArgs e)
+    {
+      v1 = Convert.ToInt32(lblResultado.Text);
+      lblResultado.Text = "";
+      lblSinal.Visible = true;
+      lblSinal.Text = "*";
+      lblInicial.Visible = true;
+      lblInicial.Text = v1.ToString();
+    }
+
+    private void btnDivisao_Click(object sender, EventArgs e)
+    {
+      v1 = Convert.ToInt32(lblResultado.Text);
+      lblResultado.Text = "";
+      lblSinal.Visible = true;
+      lblSinal.Text = "/";
+      lblInicial.Visible = true;
+      lblInicial.Text = v1.ToString();
+    }
+
+    private void pictureBox1_Click(object sender, EventArgs e)
+    {
+      Close();
+    }
+
+    private void Form1_Load(object sender, EventArgs e)
+    {
+      lblResultado.Text = "";
+      lblInicial.Visible = false;
+      lblFinal.Visible = false; 
+      lblSinal.Visible = false;
+    }
+
+  }
+}
